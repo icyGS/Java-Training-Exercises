aed9e280e6660f3c7382961778d2674931eec849
icyGS <108628576+icyGS@users.noreply.github.com>
2022-07-04 20:59:13 -0700

    day 1 stuff

---
+++ Day 1/Arrays/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] W = {"Build", "Rohan", "Optix", "Joe Biden"};
+        System.out.println(W[3]);
+        System.out.println(W[2]);
+        System.out.println(W[1]);
+        System.out.println(W[0]);
+    }
+}
---
+++ Day 1/Variables/variables.java
@@ -0,0 +1,10 @@
+public class variables {
+    int age = 15;
+    double height = 5.8;
+    char firstletter = 'k';
+    String lastname = "patil";
+    boolean programmingBest = false
+    }
+}
+
+
